f30903dd8185f9640f394c5bf660d0e094d5eff6
kmo <kmo@kmos-MacBook-Pro.local>
2023-05-11 18:03:40 -0700

    Get version number from _version.py

---
+++ evolutionary/__init__.py
@@ -0,0 +1,1 @@
+from evolutionary._version import __version__
--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
 
+with open("evolutionary/_version.py") as f:
+    version = f.readlines()[-1].split()[-1].strip("\"'")
+
+
 setup(
     name='evolutionary',
-    version='0.0.1',
+    version=version,
     author='Reuben Brasher and Emma Sawin',
-    install_requires=[],
+    install_requires=["numpy~=1.24", "scikit-learn~=1.2"],
     packages=find_packages(),
     include_package_data=True
 )
